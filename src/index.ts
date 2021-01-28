58030900ac1e4a93010b98116d5b18483fc23764
David Padron Castillo <padroncastillo@gmail.com>
2021-01-27 20:48:56 -0600

    Add mikro-orm setup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import { MikroORM } from "@mikro-orm/core"
 import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config"
 
 const main = async () => {
-  const orm  = await MikroORM.init({
-    entities: [],
-    dbName: 'reddit-clone',
-    type: 'postgresql',
-    debug: !__prod__
-  });
+  const orm  = await MikroORM.init(microConfig);
+
+  const post = orm.em.create(Post, {title: 'my first post'});
+  await orm.em.persistAndFlush(post);
+  console.log('-------------------sql 2------------')
+  await orm.em.nativeInsert(Post, {title: 'my first post 2'});
 }
 
-main();
+main().catch( (err) => {
+  console.error(err);
+});
 
 console.log('hello world!!')
