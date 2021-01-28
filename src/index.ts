d8aa9f972a3f14d9525063550c42407ece723eea
David Padron Castillo <padroncastillo@gmail.com>
2021-01-27 21:06:25 -0600

    Fix ORM failing row contains error by clearing dist

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,15 @@ import microConfig from "./mikro-orm.config"
 const main = async () => {
   const orm  = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, {title: 'my first post'});
-  await orm.em.persistAndFlush(post);
-  console.log('-------------------sql 2------------')
-  await orm.em.nativeInsert(Post, {title: 'my first post 2'});
+
+  // const post = orm.em.create(Post, {title: 'my first post'});
+  // await orm.em.persistAndFlush(post);
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts)
 }
 
 main().catch( (err) => {
   console.error(err);
 });
 
-console.log('hello world!!')
+console.log('hello world!!') 
